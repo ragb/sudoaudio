b16403f6ef7ec2dddfe184e2716f8ea4720c83d3
Rui Batista <ruiandrebatista@gmail.com>
2011-07-12 01:30:09 +0100

    Version 0.1a1pre

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sudoaudio',
-    version='0.1dev',
+    version='0.1a1pre',
     packages= find_packages(),
     license='GPL v3',
     install_requires = ['pygame'],
