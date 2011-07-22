f88178e2717c302dd28b67fb6751b6a1977232b7
Rui Batista <ruiandrebatista@gmail.com>
2011-07-22 18:52:01 +0100

    Change version to 0.1a1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sudoaudio',
-    version='0.1a1pre',
+    version='0.1a1',
     packages= find_packages(),
     license='GPL v3',
     install_requires = ['pygame'],
